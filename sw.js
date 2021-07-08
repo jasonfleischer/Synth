ddf39443a85215811f4b86ea316b142061bdd64b
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-07-08 09:17:35 -0400

    up

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
     caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/synth/',
-        '/synth/img/screen.png',
+        //'/synth/img/screen.png',
         '/synth/index.html',
         '/synth/css/main.css',
         '/synth/js/model.js',
