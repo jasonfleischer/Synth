aaa50ab24b40fb1ed1092e75e173ff80eab79d82
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-07-17 21:32:09 -0400

    up

--- js/main.js
+++ js/main.js
@@ -496,7 +496,6 @@ function setOscillatorType(type){
 
 
 function harmonicsReset() {
-	//setHarmonicVolume([1, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 		setHarmonicVolume([1, 0.5, 0.25, 0.125, 0.0625, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 }
