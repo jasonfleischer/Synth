102196b67db54891574bdca5d128b2ce92f8c085
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-06-23 20:51:11 -0400

    update

--- js/keyboard_shortcuts.js
+++ js/keyboard_shortcuts.js
@@ -3,7 +3,7 @@ function setup_keyboard_listeners() {
 
 	document.addEventListener('keyup', function(event){
 
-		//log.i(event.code)
+		log.i(event.code)
 		var code = event.code;
 		if (code === 'Space') {
 			fadeStop();
