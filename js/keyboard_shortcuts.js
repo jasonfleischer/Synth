5109b367c3c1c4ad5fd379bd92d5d6d57652ec8e
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-06-23 21:12:25 -0400

    update

--- js/keyboard_shortcuts.js
+++ js/keyboard_shortcuts.js
@@ -2,12 +2,12 @@
 function setup_keyboard_listeners() {
 	log.i("setup_keyboard_listeners")
 
-	/*window.onkeydown = function(e) {
+	window.onkeydown = function(e) {
 		console.log("?? "+e.keyCode)
     	return e.keyCode !== 32 && e.key !== " ";
 	};
 
-	document.addEventListener('keydown', function(event){
+	/*document.addEventListener('keydown', function(event){
 
 		log.i(event.code)
 		var code = event.code;
